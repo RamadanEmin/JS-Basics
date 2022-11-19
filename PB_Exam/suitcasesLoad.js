2237e2279650ed293dcaaaf3853027630e0095bd
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-11-19 08:15:42 +0200

    Add files via upload

---
+++ PB_Exam/suitcasesLoad.js
@@ -0,0 +1,27 @@
+function suitcasesLoad(input) {
+    let index = 0;
+    let capacity = Number(input[index++]);
+    let command = input[index++];
+    let counter = 0;
+    let isLoaded = true;
+    while (command !== "End") {
+        counter++;
+        let suitcases = Number(command);
+        if (counter % 3 === 0) {
+            suitcases *= 1.10;
+        }
+        capacity -= suitcases;
+        if (capacity < 0) {
+            counter--;
+            console.log(`No more space!`);
+            isLoaded = false;
+            break;
+        }
+        command = input[index++];
+    }
+    if (isLoaded) {
+        console.log(`Congratulations! All suitcases are loaded!`);
+    }
+    console.log(`Statistic: ${counter} suitcases loaded.`);
+}
+suitcasesLoad(["550", "100", "252", "72", "End"]);
